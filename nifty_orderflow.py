e391876c57e4fe6d73fc6a2a31017b55720f649f
Jacob Varghese <jacvar@gmail.com>
2026-07-31 19:41:25 +0530

    time changed

--- nifty_orderflow.py
+++ nifty_orderflow.py
@@ -971,7 +971,7 @@ def run_nifty_orderflow_scan():
             if daily_reset_date is None:
                 daily_reset_date = now_ist().date()
 
-            market_open = now.replace(hour=10, minute=00, second=0, microsecond=0)
+            market_open = now.replace(hour=9, minute=45, second=0, microsecond=0)
             market_close = now.replace(hour=15, minute=15, second=0, microsecond=0)
             if not (market_open <= now <= market_close):
                 if active_trade:
